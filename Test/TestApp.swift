8f62158a0ce3171ab62d7b749d0cb0642e266014
Vuong <vuongnq@mailinh.vn>
2020-12-23 14:31:07 +0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Vuong on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
